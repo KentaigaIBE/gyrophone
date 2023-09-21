b2bd098a797bff7ec39d343233c2d536349c0fd9
Brandon Weichert <saigabox@gmail.com>
2023-09-21 16:12:19 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+
+	let alpha: number | null = 0;
+	let beta: number | null = 0;
+	let gamma: number | null = 0;
+
+	if (browser) {
+		window.addEventListener('deviceorientation', handleOrientation);
+
+		function handleOrientation(event: DeviceOrientationEvent) {
+			alpha = event.alpha;
+			beta = event.beta;
+			gamma = event.gamma;
+		}
+	}
+</script>
+
+<h1>Gryophone</h1>
+<p>Use the power of your phone to get rotation data!</p>
+<br />
+<p>Alpha: {alpha}</p>
+<p>Beta: {beta}</p>
+<p>Gamma: {gamma}</p>
